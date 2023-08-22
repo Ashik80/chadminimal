f3b950016eaa9aab0d6a3216a4648aac72429355
Ashik80 <ashikurrahman020995@gmail.com>
2023-08-22 16:04:19 +0600

    Add keybind to stop lsp clients

--- lua/chadminimal/lsp.lua
+++ lua/chadminimal/lsp.lua
@@ -3,7 +3,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = { buffer = args.buf }
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>sc', StopActiveClients, opts)
     end,
 })
 
 vim.cmd('hi clear SignColumn')
+
+function StopActiveClients()
+    local active_clients = vim.lsp.get_active_clients()
+
+    for _, value in pairs(active_clients) do
+        vim.lsp.stop_client(value.id)
+    end
+end

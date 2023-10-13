53cfe3fadd352c3f75bebd958eb8fb22e40f2daa
Ashik80 <ashikurrahman020995@gmail.com>
2023-10-14 01:51:53 +0600

    Fix highlight for splits

--- lua/chadminimal/highlights.lua
+++ lua/chadminimal/highlights.lua
@@ -1,4 +1,5 @@
 vim.cmd('hi clear SignColumn')
 vim.cmd('hi Normal ctermbg=NONE guibg=NONE')
+vim.cmd('hi NormalNC ctermbg=NONE guibg=NONE')
 vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
 vim.cmd('hi LineNr ctermbg=NONE guibg=NONE')
